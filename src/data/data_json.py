d9158f5599164f839a221a7e8a7446dd1cfdf9c8
dtharumia <senddharshan@gmail.com>
2022-01-23 12:25:06 -0500

    database updated

--- src/data/data_json.py
+++ src/data/data_json.py
@@ -1,24 +1,34 @@
-import csv 
-import json 
+# import csv 
+# import json 
 
-def csv_to_json(csvFilePath, jsonFilePath):
-    jsonArray = []
+# def csv_to_json(csvFilePath, jsonFilePath):
+#     jsonArray = []
       
-    #read csv file
-    with open(csvFilePath, encoding='utf-8') as csvf: 
-        #load csv file data using csv library's dictionary reader
-        csvReader = csv.DictReader(csvf) 
+#     #read csv file
+#     with open(csvFilePath, encoding='utf-8') as csvf: 
+#         #load csv file data using csv library's dictionary reader
+#         csvReader = csv.DictReader(csvf) 
 
-        #convert each csv row into python dict
-        for row in csvReader: 
-            #add this python dict to json array
-            jsonArray.append(row)
+#         #convert each csv row into python dict
+#         for row in csvReader: 
+#             #add this python dict to json array
+#             jsonArray.append(row)
   
-    #convert python jsonArray to JSON String and write to file
-    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
-        jsonString = json.dumps(jsonArray, indent=4)
-        jsonf.write(jsonString)
+#     #convert python jsonArray to JSON String and write to file
+#     with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+#         jsonString = json.dumps(jsonArray, indent=4)
+#         jsonf.write(jsonString)
           
-csvFilePath = r'data.csv'
-jsonFilePath = r'data.json'
-csv_to_json(csvFilePath, jsonFilePath)
+# csvFilePath = r'data_updated.csv'
+# jsonFilePath = r'data_updated.json'
+# # csv_to_json(csvFilePath, jsonFilePath)
+
+# import hone
+
+# optional_arguments = {
+#   "delimiters": ["gt"]
+# }
+# Hone = hone.Hone(**optional_arguments)
+# schema = Hone.get_schema(csvFilePath)  # nested JSON schema for input.csv
+# result = Hone.convert(jsonFilePath, schema=schema)  # final structure, nested according to schema
+
