8e94b12d3e1d3bbc72fa5ade0b97e96415857cb9
dtharumia <senddharshan@gmail.com>
2022-01-23 13:02:17 -0500

    db fixed with unique id keys

--- src/data/data_pandas_json.py
+++ src/data/data_pandas_json.py
@@ -8,11 +8,21 @@ for i in range(0, len(df)):
     try:
         resultDict[df.iloc[i]['gt_class']]
     except:
-        resultDict[df.iloc[i]['gt_class']] = []
+        resultDict[df.iloc[i]['gt_class']] = {
+            
+        }
+for i in range(0, len(df)):
+    try:
+        resultDict[df.iloc[i]['gt_class']][int(df.iloc[i]['id'])]
+    except:
+        resultDict[df.iloc[i]['gt_class']][int(df.iloc[i]['id'])] = {
+            
+        }
+
+
 
 for i in range(0, len(df)):
-     resultDict[df.iloc[i]['gt_class']].append({
-        "id": str(df.iloc[i]['id']),
+     resultDict[df.iloc[i]['gt_class']][int(df.iloc[i]['id'])] = {
         "state": df.iloc[i]['state'],
         "term": df.iloc[i]['term'],
         "t_school": df.iloc[i]['t_school'],
@@ -22,7 +32,8 @@ for i in range(0, len(df)):
         "t_mingrade": df.iloc[i]['t_mingrade'],
         "gt_title": df.iloc[i]['gt_title'],
         "gt_ch": str(df.iloc[i]['gt_ch']),
-    })
+    }
+# print(resultDict)
 
-with open('new/data.json', 'w', encoding='utf-8') as f:
+with open('new/data2.json', 'w', encoding='utf-8') as f:
     json.dump(resultDict, f, ensure_ascii=False, indent=4)
