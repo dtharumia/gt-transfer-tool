52e43f37285df9262c9928de97724de2b391f7e0
dtharumia <senddharshan@gmail.com>
2022-01-23 00:28:35 -0500

    added more sample transfer data

--- src/data/data_json.py
+++ src/data/data_json.py
@@ -1,3 +1,24 @@
-import pandas
-df = pandas.rea
-pandas.read_csv('data.csv').to_json('data.json')
+import csv 
+import json 
+
+def csv_to_json(csvFilePath, jsonFilePath):
+    jsonArray = []
+      
+    #read csv file
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+  
+    #convert python jsonArray to JSON String and write to file
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+          
+csvFilePath = r'data.csv'
+jsonFilePath = r'transfer.js'
+csv_to_json(csvFilePath, jsonFilePath)
