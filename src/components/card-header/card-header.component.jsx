c520c78124c1f34c18bab6a8a895babd960cb2be
dtharumia <senddharshan@gmail.com>
2022-01-22 22:33:30 -0500

    adding new transfer test data

---
+++ src/components/card-header/card-header.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './card-header.styles.scss';
+
+const CardHeader = () => {
+    <div className='card-header'>
+        CARD HEADER
+    </div>
+}
+
+export default CardHeader;
---
+++ src/data/data_json.py
@@ -0,0 +1,3 @@
+import pandas
+df = pandas.rea
+pandas.read_csv('data.csv').to_json('data.json')
