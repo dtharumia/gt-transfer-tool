7c4b28d2d07a469e37cee4705d318c1e50579214
dharshan <senddharshan@gmail.com>
2023-03-23 13:49:06 -0400

    testing test_scrape

--- public/data/combine_state_output.py
+++ public/data/combine_state_output.py
@@ -9,18 +9,24 @@ folder_path = sys.argv[1]
 def combine_json():
     # get all json files in data folder
     path = os.path.join(sys.path[0], f'output/{folder_path}')
-    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith('.json')]
+    df = pd.DataFrame()
+
+    # go through all the subfolders and get all the json files
+    files = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.json' in file:
+                files.append(os.path.join(r, file))
 
     # combine all json files into one df
-    df = pd.DataFrame()
     for file in files:
         df = df.append(pd.read_json(os.path.join(path, file), orient='records'), ignore_index=True)
-
+    
     # replace id with index
     df.drop('id', axis=1, inplace=True)
     df.insert(0, 'id', range(0, 0 + len(df)))
 
-    # save combined df as json
+    # # save combined df as json
     df.to_json(os.path.join(sys.path[0], f'output/{folder_path}/{folder_path}_combined.json'), orient='records')
 
 if __name__ == '__main__':
