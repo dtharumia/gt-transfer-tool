33d85990b926a9751d1c3f5fc8e024f9227aae24
dharshan <senddharshan@gmail.com>
2023-04-03 01:14:57 -0400

    testing keyboard fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,7 @@ export default function Home() {
           <Search></Search>
         </Center>
       </Box>
-      <Box minH={'5vh'}>
+      <Box minH={'250'}>
         {!haveHits && (
           <HStack
             spacing="20px"
